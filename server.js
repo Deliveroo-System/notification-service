a3457bd3e3f00cfdae13f7923db842ae363ba30b
Sonadi <144614600+Sonadi@users.noreply.github.com>
2025-04-26 19:24:03 +0530

    add

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 
 app.use('/api/notifications', notificationRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5056;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
