36f4577cc56f48001cac9a56d9420f0f4a3027f5
vidura-12 <viduranirmal195@gmail.com>
2025-04-27 19:54:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const notificationRoutes = require('./routes/notificationRoutes');
 
 const app = express();
 
-
 app.use(cors()); // Allow all origins by default
-
 app.use(bodyParser.json());
 
 app.use('/api/notifications', notificationRoutes);
 
 const PORT = process.env.PORT || 5056;
-app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+server.setTimeout(50000); // Set timeout to 50 seconds
+
